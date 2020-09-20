ec89ce4ef018d8142ef67bdb803bfb21cbb55adc
Stavros <stavroschios@gmail.com>
2020-09-20 01:28:41 -0400

    Initial commit

---
+++ src/datastructures/one/Main.java
@@ -0,0 +1,7 @@
+package datastructures.one;
+
+public class Main {
+    public static void main(String[] names) {
+    }
+}
+
---
+++ src/datastructures/three/MinPriorityQueue.java
@@ -0,0 +1,17 @@
+package datastructures.three;
+
+public class MinPriorityQueue {
+    private MinHeap heap = new MinHeap();
+
+    public void add(String value, int priority) {
+        heap.insert(priority, value);
+    }
+
+    public String remove() {
+        return heap.remove();
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+}
